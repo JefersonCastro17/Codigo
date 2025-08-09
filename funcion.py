4a3745d72ecf9fd2ca3bd1dc2b99287344293d85
JefersonCastro17 <jefersonjairbernalcastro172129@gmail.com>
2025-08-09 00:25:06 -0500

    logica de multiplicar

---
+++ funcion.py
@@ -0,0 +1,17 @@
+def multiplicacion():
+    a = float(input("Ingrese un número: "))
+    b = float(input("Ingrese el siguiente número: "))
+    print(f"El resultado es: {a * b}")
+
+# Las demás funciones vacías para que el menú no falle
+def suma():
+    print("Función no encontrada")
+
+def resta():
+    print("Función no encontrada")
+
+def division():
+    print("Función no encontrada")
+
+def modulo():
+    print("Función no encontrada")
